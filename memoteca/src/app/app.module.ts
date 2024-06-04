a65c3c6483ac8ea453ed80b00f185b81ddba606b
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-04 15:52:46 -0300

    Associando com Event Binding

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { DesafioTesteComponent } from './componentes/desafio-teste/desafio-teste.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    DesafioTesteComponent
+    CriarPensamentoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
