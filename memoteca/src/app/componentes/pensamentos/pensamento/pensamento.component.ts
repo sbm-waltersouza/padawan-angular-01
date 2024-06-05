65eb9613e2aed2bcac982fca83034a58eeadbadf
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-05 14:43:24 -0300

    Comunicação entre os componentes

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
-  pensamento = {
-    conteudo: 'I love angular',
+  @Input( ) pensamento = {
+    conteudo: 'Comunicação entre componentes',
     autoria: 'Nay',
     modelo: 'modelo3'
   }
