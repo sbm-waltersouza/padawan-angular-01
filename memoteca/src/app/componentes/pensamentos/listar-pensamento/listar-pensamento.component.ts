0f3adf6fadff4badbd351517dd91dc0ccb551823
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-05 15:19:15 -0300

    Estilizando com ngClass

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,7 +17,12 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Minha propriedade é decorada com @input()',
       autoria: 'componente filho',
       modelo: 'modelo2'
-    }
+    },
+    {
+      conteudo: 'lorem Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: '',
+      modelo: 'modelo1'
+  },
   ];
 
   constructor() { }
